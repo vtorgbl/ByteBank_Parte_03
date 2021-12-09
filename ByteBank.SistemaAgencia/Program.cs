9a799cda2ed9c804e4f2aca55829e5851924db14
Rafael de Paula <rafael.paula@netviewinformatica.com.br>
2021-12-09 13:38:10 -0300

    Add project files.

---
+++ ByteBank.SistemaAgencia/Program.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using ByteBank.Modelos;
+using ByteBank.Modelos.Funcionarios;
+
+namespace ByteBank.SistemaAgencia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá Mundo");
+            Console.WriteLine(123);
+            Console.WriteLine(10.5);
+            Console.WriteLine(true);
+
+            object conta = new ContaCorrente(456, 687876);
+            object desenvolvedor = new Desenvolvedor("4564654");
+
+            string contaToString = conta.ToString();
+
+            Console.WriteLine("Resultado do contaToString: " + contaToString);
+            Console.WriteLine(conta);
+
+            Console.ReadLine();
+
+            Cliente carlos_1 = new Cliente();
+            carlos_1.Nome = "Carlos";
+            carlos_1.CPF = "458.623.120-03";
+            carlos_1.Profissao = "Designer";
+
+            Cliente carlos_2 = new Cliente();
+            carlos_2.Nome = "Carlos";
+            carlos_2.CPF = "458.623.120-03";
+            carlos_2.Profissao = "Designer";
+
+            if (carlos_1.Equals(carlos_2)) { Console.WriteLine("São iguais"); }
+            Console.WriteLine("Não são iguais");
+
+            Console.ReadLine();
+
+        }
+
+        static void TestaString()
+        {
+            // pagina?argumentos
+            // 012345678
+
+            string urlTeste = "http://www.butebank.com/cambio";
+            int indiceByteBank = urlTeste.IndexOf("http://www.butebank.com");
+
+            // Olá, meu nome é Guilherme e você pode entrar em contato comigo
+            // através do número 8457-4456!
+
+            //string padrao = "[0123456789][0123456789][0123456789][0123456789][-][0123456789][0123456789][0123456789][0123456789]"; 
+            //string padrao = "[0-9][0-9][0-9][0-9][-][0-9][0-9][0-9][0-9]";
+            //string padrao = "[0-9]{4}[-][0-9]{4}";
+            //string padrao = "[0-9]{4,5}[-]{0,1}[0-9]{4}";
+            //string padrao = "[0-9]{4,5}-{0,1}[0-9]{4}";
+            string padrao = "[0-9]{4,5}-?[0-9]{4}";
+
+            string textoTeste = "Meu nome é Guilherme, me ligue em 4748-4546";
+            Console.WriteLine(Regex.IsMatch(textoTeste, padrao));
+
+            Match resultado = Regex.Match(textoTeste, padrao);
+            Console.WriteLine(resultado.Value);
+
+            Console.ReadLine();
+
+            //---------------------------------------------------------------
+            Console.WriteLine(urlTeste.StartsWith("http://www.butebank.com"));
+            Console.WriteLine(urlTeste.EndsWith("cambio/"));
+
+            Console.WriteLine(urlTeste.Contains("ByteBank"));
+
+            Console.ReadLine();
+
+            //moedaOrigem=real&moedaDestino=dolar
+            //          |
+            //  -------´
+
+            string urlParametros = "http://www.bytebank.com/cambio?moedaOrigem=real&moedaDestino=dolar&valor=1500";
+            ExtratorValorDeArgumentosURL extratorDeValores = new ExtratorValorDeArgumentosURL(urlParametros);
+
+            string valor = extratorDeValores.GetValor("moedaDestino");
+            Console.WriteLine("Valor de moedaDestino: " + valor);
+
+            string valorMoedaOrigem = extratorDeValores.GetValor("moedaOrigem");
+            Console.WriteLine("Valor de moedaOrigem: " + valorMoedaOrigem);
+
+            Console.WriteLine(extratorDeValores.GetValor("VALOR"));
+
+            Console.ReadLine();
+
+
+
+            //Testando ToLower
+            string mensagemOrigem = "PALAVRA";
+            string termoBusca = "ra";
+
+            Console.WriteLine(mensagemOrigem.ToLower());
+
+
+            // Testando Replace
+            termoBusca = termoBusca.Replace('r', 'R');
+            Console.WriteLine(termoBusca);
+
+            termoBusca = termoBusca.Replace('a', 'A');
+            Console.WriteLine(termoBusca);
+
+            Console.WriteLine(mensagemOrigem.IndexOf(termoBusca));
+            Console.ReadLine();
+
+            // Testando o método Remove
+            string testeRemocao = "primeiraParte&123456789";
+            int indiceEComercial = testeRemocao.IndexOf('&');
+            Console.WriteLine(testeRemocao.Remove(indiceEComercial, 4));
+            Console.ReadLine();
+
+
+            // <nome>=<valor>
+            string palavra = "moedaOrigem=moedaDestino&moedaDestino=dolar";
+            string nomeArgumento = "moedaDestino=";
+
+            int indice = palavra.IndexOf(nomeArgumento);
+            Console.WriteLine(indice);
+
+            Console.WriteLine("Tamanho da string nomeArgumento: " + nomeArgumento.Length);
+
+            Console.WriteLine(palavra);
+            Console.WriteLine(palavra.Substring(indice));
+            Console.WriteLine(palavra.Substring(indice + nomeArgumento.Length));
+            Console.ReadLine();
+
+
+
+
+
+            // Testando o IsNullOrEmpty
+            string textoVazio = "";
+            string textoNulo = null;
+            string textoQualquer = "kjhfsdjhgsdfjksdf";
+            Console.WriteLine(String.IsNullOrEmpty(textoVazio));
+            Console.WriteLine(String.IsNullOrEmpty(textoNulo));
+            Console.WriteLine(String.IsNullOrEmpty(textoQualquer));
+            Console.ReadLine();
+
+
+
+
+
+
+            ExtratorValorDeArgumentosURL extrator = new ExtratorValorDeArgumentosURL("");
+
+            string url = "pagina?moedaOrigem=real&moedaDestino=dolar";
+
+            int indiceInterrogacao = url.IndexOf('?');
+
+            Console.WriteLine(indiceInterrogacao);
+
+            Console.WriteLine(url);
+            string argumentos = url.Substring(indiceInterrogacao + 1);
+            Console.WriteLine(argumentos);
+
+
+            Console.ReadLine();
+        }
+    }
+
+
+}
